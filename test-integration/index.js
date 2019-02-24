7a3f6845611a5f3ad0563b90997dde5aedcfe536
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-02-23 22:00:20 -0500

    Removing strict due to nodejs version

--- test-integration/index.js
+++ test-integration/index.js
@@ -1,16 +1,16 @@
 const STAGE_ROUTE = process.env.STAGE_ROUTE;
 const EVENTS_ROUTE = `${STAGE_ROUTE}-events`;
 
-const assert = require('assert').strict;
+const assert = require('assert');
 const request = require('request-promise-native');
 
 async function test() {
   try {
     const upcomingEventsResponse = await request(`${EVENTS_ROUTE}/upcoming`);
-    assert.strictEqual(upcomingEventsResponse.statusCode, 200);
+    assert.equal(upcomingEventsResponse.statusCode, 200);
 
     const { events: upcomingEvents } = await upcomingEventsResponse.json();
-    assert.strictEqual(Array.isArray(upcomingEvents), true);
+    assert.equal(Array.isArray(upcomingEvents), true);
   } catch (error) {
     console.error(error);
     process.exit(1);
