d21e78e0a4d959b1bef9ea8bf5430f62bf1ed00a
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-02-23 21:42:14 -0500

    Adding integration test for upcoming endpoint

--- test-integration/index.js
+++ test-integration/index.js
@@ -1,2 +1,20 @@
-console.log('hello workflow');
-console.log(process.env.GITHUB_SHA);
+const STAGE_ROUTE = process.env.STAGE_ROUTE;
+const EVENTS_ROUTE = `${STAGE_ROUTE}-events`;
+
+const assert = require('assert').strict;
+const request = require('request-promise-native');
+
+async function test() {
+  try {
+    const upcomingEventsResponse = await request(`${EVENTS_ROUTE}/upcoming`);
+    assert.strictEqual(upcomingEventsResponse.statusCode, 200);
+
+    const { events: upcomingEvents } = await upcomingEventsResponse.json();
+    assert.strictEqual(Array.isArray(upcomingEvents), true);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+test();
