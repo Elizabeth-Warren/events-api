7f29fdf660bc0358c5804ee2d43da783d3ea1df4
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-02-24 16:10:43 -0500

    Debugging integration test

--- test-integration/index.js
+++ test-integration/index.js
@@ -7,6 +7,7 @@ const request = require('request-promise-native');
 async function test() {
   try {
     const upcomingEventsResponse = await request(`${EVENTS_ROUTE}/upcoming`);
+    console.log(upcomingEventsResponse);
     assert.equal(upcomingEventsResponse.statusCode, 200);
 
     const { events: upcomingEvents } = await upcomingEventsResponse.json();
